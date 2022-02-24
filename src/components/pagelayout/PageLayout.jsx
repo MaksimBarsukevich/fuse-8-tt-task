a87fabede7018fe161fc23da81b6f4dd3f3ab711
Maxim <maksikprado@gmail.com>
2022-02-24 15:01:20 +0300

    initial

---
+++ src/components/pagelayout/PageLayout.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import "./pagelayout.sass";
+
+
+
+const PageLayout = () => {
+ 
+
+  return (
+    <main className="contacts-page">
+      <section className="contacts-page__wrapper">
+        Привет
+      </section>
+    </main>
+  );
+};
+
+export default PageLayout;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import PageLayout from './components/pagelayout/PageLayout.jsx';
+
+render(<PageLayout/>, document.getElementById('root'));
